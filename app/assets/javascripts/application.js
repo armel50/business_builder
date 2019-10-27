e75d24c361cb1a70ff53dedb551ab7eada3c3a2b
Armel <armelfli50@gmail.com>
2019-10-27 17:38:03 -0500

    set up semantic ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+
+    // fix menu when passed
+    $('.masthead').visibility({
+        once: false,
+        onBottomPassed: function() {
+          $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+          $('.fixed.menu').transition('fade out');
+        }
+      });
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+
+  });
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :businesses
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   root "application#home"
 end
